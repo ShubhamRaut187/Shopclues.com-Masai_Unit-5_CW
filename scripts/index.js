126a61852328ce7e9bb5d0eef8ed52f8896be963
ShubhamRaut187 <raut.shubham4554@gmail.com>
2023-04-12 14:36:23 +0530

    Navbar Added with CSS

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+import navbar from "../components/navbar.js";
+let n = document.getElementById("navbar");
+n.innerHTML = navbar();
+
+let slideshow_img = [
+    "https://cdn.shopclues.com/images/banners/2023/Mar/21/HB1_BestOfFootwear_Web_SYM_21Mar23.jpg",
+    "https://cdn.shopclues.com/images/banners/2023/Mar/25/HB2_Refurbished_Web_SYM_25Mar23.jpg",
+    "https://cdn.shopclues.com/images/banners/2023/Mar/04/HB4_Aromapure_Web_SYM_04Mar23.jpg",
+    "https://cdn.shopclues.com/images/banners/2023/Mar/29/HB4_Web_App_SYM_29Mar23.jpg",
+    "https://cdn.shopclues.com/images/banners/2023/Mar/04/HB5_Asus_Web_SYM_04Mar23.jpg"
+]
