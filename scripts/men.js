4ba2b0c0b3414e0876cd84edcabdbb1682ca6f7f
ShubhamRaut187 <raut.shubham4554@gmail.com>
2023-04-15 02:55:49 +0530

    HomePage,Navbar,Footer Complete SignUp added

--- scripts/men.js
+++ scripts/men.js
@@ -1,8 +1,8 @@
 import navbar from "../components/navbar.js";
 import footer from "../components/footer.js";
 
-document.getElementById("navbar").innerHTML = navbar;
-document.getElementById("footer").innerHTML = footer;
+document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
 
 let carousalContainer = document.getElementById("carousal");
 window.onload = slideshow();
