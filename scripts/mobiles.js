b3dc71bbb5111a03e0280f619db398467c715d7e
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-07-10 20:16:14 +0530

    Mobile Page responsive

--- scripts/mobiles.js
+++ scripts/mobiles.js
@@ -2,7 +2,7 @@ import navbar from "../components/navbar.js";
 import footer from "../components/footer.js";
 
 document.getElementById("navbar").innerHTML = navbar();
-document.getElementById("footer").innerHTML = footer();
+document.getElementById("footerM").innerHTML = footer();
 
 let productList = document.getElementById("products-list");
 
